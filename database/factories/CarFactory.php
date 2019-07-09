88052a264523d5b6a3d419fa1b2d525135a61b26
Vuk Kapor <vukkapor12@gmail.com>
2019-07-09 20:13:52 +0200

    car seeder added

---
+++ database/factories/CarFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+use App\Car;
+
+$factory->define(Car::class, function (Faker $faker) {
+    return [
+        'brand' => $faker->randomElement($array = array ('BMW', 'Mercedes', 'Toyota', 'Hyundai')),
+        'model' => $faker->word,
+        'year' => $faker->date($format = 'Y-m-d', $max = 'now'),
+        'maxSpeed' => $faker->numberBetween($min = 60, $max = 300),
+        'isAutomatic' => $faker->boolean($chanceOfGettingTrue = 50),
+        'engine' => $faker->randomElement($array = array ('diesel', 'petrol', 'electric', 'hybrid')),
+        'numberOfDoors' => $faker->randomElement($array = array (3, 5, 7))
+    ];
+});
